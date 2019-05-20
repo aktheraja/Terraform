d64cd0d5b65d726bb977284cc91d030435a0a57f
aktheraja <wale.akin2007@gmail.com>
2019-05-19 23:19:15 -0600

    updated

--- resources.tf
+++ resources.tf
@@ -26,3 +26,34 @@ resource "aws_security_group" "subnetsecurity" {
 	}
 }
 
+data "aws_ami" "ubuntu" {
+	most_recent = true
+
+	filter {
+		name = "name"
+		values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+	}
+
+	filter {
+		name = "virtualization-type"
+		values= ["hvm"]
+	}
+
+	owners = ["099720109477"]
+}
+
+resource "aws_instance" "secondserver" {
+	count         = "${var.instance_count}"
+	ami = "${data.aws_ami.ubuntu.id}"
+	instance_type = "t2.micro"
+
+	tags {
+		Name = "identifiertag"
+	}
+
+	subnet_id = "${aws_subnet.subnet2.id}"
+}
+
+variable "instance_count" {
+	default = "3"
+}
