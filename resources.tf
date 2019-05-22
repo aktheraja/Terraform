db9455ab5cfd6cdd26e7019d305256319655c3dc
aktheraja <wale.akin2007@gmail.com>
2019-05-21 21:08:25 -0600

    update added

--- resources.tf
+++ resources.tf
@@ -8,6 +8,7 @@ resource "aws_subnet" "subnet1" {
 cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block,3,1)}"
 	vpc_id = "${aws_vpc.environment-example-two.id}"
 	availability_zone = "us-east-1a"
+
 }
 resource "aws_subnet" "subnet2" {
 	cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block,2,2)}"
@@ -24,36 +25,52 @@ resource "aws_security_group" "subnetsecurity" {
 		protocol = "tcp"
 		to_port = 80
 	}
+	ingress {
+		cidr_blocks = ["${aws_vpc.environment-example-two.cidr_block}"]
+		from_port = 22
+		protocol = "tcp"
+		to_port = 22
+	}
+
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
 }
 
 data "aws_ami" "ubuntu" {
 	most_recent = true
-
 	filter {
 		name = "name"
 		values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
 	}
-
 	filter {
 		name = "virtualization-type"
 		values= ["hvm"]
 	}
-
 	owners = ["099720109477"]
 }
 
-resource "aws_instance" "secondserver" {
-	count         = "${var.instance_count}"
-	ami = "${data.aws_ami.ubuntu.id}"
-	instance_type = "t2.micro"
-
-	tags {
-		Name = "identifiertag"
-	}
-
-	subnet_id = "${aws_subnet.subnet2.id}"
+resource "aws_key_pair" "my" {
+	key_name   = "my"
+	public_key = "${file("C:/Users/akfre/Downloads/SSH/my.pub")}"
 }
 
+resource "aws_instance" "my-instance" {
+	ami = "ami-04169656fea786776"
+	count = "${var.instance_count}"
+	instance_type = "t2.micro"
+	key_name = "${aws_key_pair.my.key_name}"
+	user_data = "${file("C:/Users/akfre/OneDrive/Documents/terraform bash/install_apache.sh")}"
+tags = {
+Name = "Terraform"
+Batch = "5AM"
+}
+}
 variable "instance_count" {
 	default = "3"
 }
+
+
