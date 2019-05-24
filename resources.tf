7f92081bb5beec77856cc2df15e25f258c21ff0e
aktheraja <wale.akin2007@gmail.com>
2019-05-24 11:44:37 -0600

    igw and apache installed

--- resources.tf
+++ resources.tf
@@ -8,6 +8,9 @@ resource "aws_subnet" "subnet1" {
 cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block,3,1)}"
 	vpc_id = "${aws_vpc.environment-example-two.id}"
 	availability_zone = "us-east-1a"
+	map_public_ip_on_launch = true
+
+	depends_on = ["aws_internet_gateway.default"]
 
 }
 resource "aws_subnet" "subnet2" {
@@ -20,13 +23,13 @@ resource "aws_subnet" "subnet2" {
 resource "aws_security_group" "subnetsecurity" {
 	vpc_id = "${aws_vpc.environment-example-two.id}"
 	ingress {
-		cidr_blocks = ["${aws_vpc.environment-example-two.cidr_block}"]
+		cidr_blocks = ["0.0.0.0/0"]
 		from_port = 80
 		protocol = "tcp"
 		to_port = 80
 	}
 	ingress {
-		cidr_blocks = ["${aws_vpc.environment-example-two.cidr_block}"]
+		cidr_blocks = ["0.0.0.0/0"]
 		from_port = 22
 		protocol = "tcp"
 		to_port = 22
@@ -45,6 +48,7 @@ data "aws_ami" "ubuntu" {
 	filter {
 		name = "name"
 		values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+
 	}
 	filter {
 		name = "virtualization-type"
@@ -53,24 +57,57 @@ data "aws_ami" "ubuntu" {
 	owners = ["099720109477"]
 }
 
-resource "aws_key_pair" "my" {
-	key_name   = "my"
-	public_key = "${file("C:/Users/akfre/Downloads/SSH/my.pub")}"
-}
+//resource "aws_key_pair" "my" {
+//	key_name   = "my"
+//	public_key = "${file("C:/Users/akfre/Downloads/SSH/MyKP.pub")}" }
 
+
+//instances
+variable "ami_key_pair_name" {}
 resource "aws_instance" "my-instance" {
 	ami = "ami-04169656fea786776"
 	count = "${var.instance_count}"
 	instance_type = "t2.micro"
-	key_name = "${aws_key_pair.my.key_name}"
-	user_data = "${file("C:/Users/akfre/OneDrive/Documents/terraform bash/install_apache.sh")}"
+//	key_name = "${aws_key_pair.my.key_name}"
+	user_data = "${file("C:/Users/akfre/OneDrive/Documents/terraform bash/install_apache2.sh")}"
+	security_groups = ["${aws_security_group.subnetsecurity.id}"]
+	subnet_id = "${aws_subnet.subnet1.id}"
+	key_name = "${var.ami_key_pair_name}"
 tags = {
 Name = "Terraform"
-Batch = "5AM"
-}
-}
-variable "instance_count" {
-	default = "3"
 }
 
+}
+variable "instance_count" {
+	default = "1"
+}
+
+//internet gateway
+resource "aws_internet_gateway" "default" {
+	vpc_id = "${aws_vpc.environment-example-two.id}"
+}
+
+//routing table for pubic subnet
+resource "aws_route_table" "eu-west-1a-public" {
+	vpc_id = "${aws_vpc.environment-example-two.id}"
+
+	route {
+		cidr_block = "0.0.0.0/0"
+		gateway_id = "${aws_internet_gateway.default.id}"
+	}
+
+	tags {
+		Name = "Public Subnet Routing Table"
+	}
+}
+//route table association
+resource "aws_route_table_association" "eu-west-1a-public" {
+	subnet_id = "${aws_subnet.subnet1.id}"
+	route_table_id = "${aws_route_table.eu-west-1a-public.id}"
+}
+
+resource "aws_eip" "nat" {
+	instance = "${aws_instance.my-instance.id}"
+	vpc = true
+}
 
