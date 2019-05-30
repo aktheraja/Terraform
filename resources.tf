11b1e00bbf3612ee82f3567d3f03728f8ad9b8e8
Craig Martis <craigmartis@yahoo.ca>
2019-05-30 14:37:33 -0600

    Added Routing table corrections

--- resources.tf
+++ resources.tf
@@ -2,7 +2,6 @@ resource "aws_vpc" "vpc_environment" {
 	cidr_block= "10.0.0.0/16"
 	enable_dns_hostnames = true
 	enable_dns_support = true
-	tags{Name = "terraform-aws-vpc"}
 }
 resource "aws_subnet" "public_subnet1" {
 cidr_block = "${cidrsubnet(aws_vpc.vpc_environment.cidr_block,3,1)}"
@@ -97,37 +96,44 @@ variable "ami_key_pair_name" {}
 
 
 //internet gateway
-resource "aws_internet_gateway" "default" {
+resource "aws_internet_gateway" "default_gat" {
 	vpc_id = "${aws_vpc.vpc_environment.id}"
 }
 
-//routing table for pubic subnet
-resource "aws_route_table" "route-public" {
+//routing table for pubic subnet 1
+resource "aws_route_table" "route-public_1" {
 	vpc_id = "${aws_vpc.vpc_environment.id}"
-
+	subnet_id = "${aws_subnet.public_subnet1.id}"
 	route {
-		cidr_block = "0.0.0.0/0"
-		gateway_id = "${aws_internet_gateway.default.id}"
+		cidr_block = "${aws_subnet.public_subnet1.cidr_block}"
+		gateway_id = "${aws_internet_gateway.default_gat.id}"
 	}
 
 	tags {
-		Name = "Public Subnet Routing Table"
+		Name = "Public Subnet 1 Routing Table"
 	}
 }
-//route table association
-resource "aws_route_table_association" "route_association_public" {
-	subnet_id = "${aws_subnet.public_subnet1.id}"
-	route_table_id = "${aws_route_table.route-public.id}"
+
+//routing table for pubic subnet 2
+resource "aws_route_table" "route-public_2" {
+	vpc_id = "${aws_vpc.vpc_environment.id}"
+	subnet_id = "${aws_subnet.public_subnet2.id}"
+	route {
+		cidr_block = "${aws_subnet.public_subnet2.cidr_block}"
+		gateway_id = "${aws_internet_gateway.default_gat.id}"
+	}
+
+	tags {
+		Name = "Public Subnet 2 Routing Table"
+	}
 }
 
-
-//
 resource "aws_launch_configuration" "autoscale_launch" {
 	name                 = "CF2TF-LC"
 	image_id             = "ami-14c5486b"
 	instance_type        = "t2.nano"
 	key_name              = "${var.ami_key_pair_name}"
-	security_groups      = ["${aws_security_group.subnetsecurity.id}"]
+	security_groups      = ["${aws_security_group.private_subnetsecurity.id}"]
 	user_data = "${file("C:/Users/Default.Default-PC/Downloads/install_apache_server.sh")}"
 }
 
@@ -175,7 +181,7 @@ resource "aws_autoscaling_attachment" "alb_autoscale" {
 resource "aws_lb" "alb" {
 	name            = "alb"
 	subnets         = ["${aws_subnet.public_subnet1.id}","${aws_subnet.private_subnet1.id}"]
-	security_groups = ["${aws_security_group.subnetsecurity.id}"]
+	security_groups = ["${aws_security_group.private_subnetsecurity.id}", "${aws_security_group.public_subnetsecurity.id}"]
 	internal        = false
 	idle_timeout    = 60
 	tags {
@@ -191,18 +197,6 @@ resource "aws_lb_listener" "alb_listener" {
 		type             = "forward"
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 resource "aws_autoscaling_group" "autoscale_group_2" {
 	launch_configuration = "${aws_launch_configuration.autoscale_launch.id}"
@@ -245,34 +239,21 @@ resource "aws_autoscaling_attachment" "alb_autoscale_2" {
 	autoscaling_group_name = "${aws_autoscaling_group.autoscale_group_2.id}"
 }
 
-resource "aws_lb" "alb_2" {
-	name            = "alb"
-	subnets         = ["${aws_subnet.public_subnet2.id}","${aws_subnet.private_subnet2.id}"]
-	security_groups = ["${aws_security_group.subnetsecurity.id}"]
-	internal        = false
-	idle_timeout    = 60
-	tags {
-		Name    = "alb"
-	}
+
+resource "aws_nat_gateway" "nat_2gate" {
+	allocation_id = "${aws_eip.nat_eip.id}"
+	subnet_id = "${aws_subnet.public_subnet2.id}"
 }
-resource "aws_lb_listener" "alb_listener_2" {
-	load_balancer_arn = "${aws_lb.alb_2.arn}"
-	port              = 80
-	protocol          = "HTTP"
-	default_action {
-		target_group_arn = "${aws_lb_target_group.alb_target_group_2.arn}"
-		type             = "forward"
-	}
+
+resource "aws_nat_gateway" "nat_1gate" {
+	allocation_id = "${aws_eip.nat_eip.id}"
+	subnet_id = "${aws_subnet.public_subnet1.id}"
 }
-resource "aws_nat_gateway" "nat_gate" {
-	allocation_id = ""
-	subnet_id = ""
-}
-resource "aws_eip" "nat" {
+
+
+resource "aws_eip" "nat_eip" {
 	vpc =  true
 
 	instance = "${aws_launch_configuration.autoscale_launch.image_id}"
 	depends_on = ["aws_internet_gateway.default"]
-
-
 }
