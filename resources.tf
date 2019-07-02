a0a53367a9cc35ef7df40976a1225e1289cd9f49
Yujia Cui <yujia.cui@ucalgary.ca>
2019-07-02 15:47:00 -0600

    Nikky: Working version

--- resources.tf
+++ resources.tf
@@ -113,7 +113,7 @@ resource "aws_internet_gateway" "default_gat" {
 //routing table for public subnets
 resource "aws_route_table" "route-public" {
   vpc_id = aws_vpc.vpc_environment.id
-depends_on = [aws_vpc.vpc_environment]
+//depends_on = [aws_vpc.vpc_environment]
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.default_gat.id
@@ -127,7 +127,7 @@ depends_on = [aws_vpc.vpc_environment]
 //routing table for private subnet 1
 resource "aws_route_table" "route-private1" {
   vpc_id = aws_vpc.vpc_environment.id
-  depends_on = [aws_subnet.private_subnet1]
+//  depends_on = [aws_subnet.private_subnet1]
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_nat_gateway.nat_1gate.id
@@ -141,7 +141,7 @@ resource "aws_route_table" "route-private1" {
 //routing table for private subnet 2
 resource "aws_route_table" "route-private2" {
   vpc_id = aws_vpc.vpc_environment.id
-  depends_on = [aws_subnet.private_subnet2]
+//  depends_on = [aws_subnet.private_subnet2]
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -168,39 +168,83 @@ resource "aws_route_table_association" "subnet2_table_assoc" {
 }
 
 resource "aws_launch_configuration" "autoscale_launch" {
-  name            = "CF2TF-LC"
+  name            = "CF2TF-LC3"
   image_id        = "ami-07669fc90e6e6cc47"
   instance_type   = "t2.nano"
-  //key_name        = var.ami_key_pair_name
+//  key_name        = var.ami_key_pair_name
   security_groups = [aws_security_group.security.id]
   enable_monitoring = true
   user_data = file(
-    "C:/Users/Default.Default-PC/Downloads/install_apache_server.sh"
+    "/Users/yujiacui/Desktop/install_apache_server2.sh"
   )
+  lifecycle {create_before_destroy = true}
 }
 
 resource "aws_autoscaling_group" "autoscale_group_1" {
+  name="asg-${aws_launch_configuration.autoscale_launch.name}"
   launch_configuration = aws_launch_configuration.autoscale_launch.id
   vpc_zone_identifier  = [aws_subnet.private_subnet2.id, aws_subnet.private_subnet1.id]
-  enabled_metrics = []
+//  enabled_metrics = []
   //	load_balancers = ["${aws_elb.elb.name}"]
-  min_size = 3
-  max_size = 3
+  min_size = 2
+  max_size = 5
+  desired_capacity = 3
+  wait_for_elb_capacity = 3
   tag {
     key                 = "Name"
     value               = "auto_scale"
     propagate_at_launch = true
   }
+  health_check_grace_period = 300
+  health_check_type = "ELB"
+  lifecycle {create_before_destroy = true}
+  enabled_metrics = [
+    "GroupMinSize",
+    "GroupMaxSize",
+    "GroupDesiredCapacity",
+    "GroupInServiceInstances",
+    "GroupTotalInstances"
+  ]
+  metrics_granularity="1Minute"
+
+}
+
+resource "aws_autoscaling_policy" "web_policy_up" {
+  name = "web_policy_up"
+  scaling_adjustment = 1
+  adjustment_type = "ChangeInCapacity"
+  cooldown = 300
+  autoscaling_group_name = "${aws_autoscaling_group.autoscale_group_1.name}"
+//  autoscaling_group_name = "${aws_autoscaling_group.web.name}"
+}
+
+resource "aws_cloudwatch_metric_alarm" "web_cpu_alarm_up" {
+  alarm_name = "web_cpu_alarm_up"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods = "2"
+  metric_name = "CPUUtilization"
+  namespace = "AWS/EC2"
+  period = "120"
+  statistic = "Average"
+  threshold = "60"
+
+  dimensions = {
+    AutoScalingGroupName = "${aws_autoscaling_group.autoscale_group_1.name}"
+//    "${aws_autoscaling_group.web.name}"
+  }
+
+  alarm_description = "This metric monitor EC2 instance CPU utilization"
+  alarm_actions = ["${aws_autoscaling_policy.web_policy_up.arn}"]
 }
 
 resource "aws_alb_target_group" "alb_target_group_1" {
-  name     = "alb-target-group"
+  name     = "alb-target-group2"
   port     = "80"
   protocol = "HTTP"
   vpc_id   = aws_vpc.vpc_environment.id
 
   tags = {
-    name = "alb_target_group"
+    name = "alb_target_group2"
   }
   stickiness {
     type            = "lb_cookie"
@@ -224,13 +268,13 @@ resource "aws_autoscaling_attachment" "alb_autoscale" {
 }
 
 resource "aws_alb" "alb" {
-  name            = "alb"
+  name            = "alb2"
   subnets         = [aws_subnet.public_subnet1.id, aws_subnet.public_subnet2.id]
   security_groups = [aws_security_group.security.id]
   internal        = false
   idle_timeout    = 60
   tags = {
-    Name = "alb"
+    Name = "alb2"
   }
 }
 
@@ -319,4 +363,33 @@ resource "aws_cloudwatch_log_metric_filter" "viewer_view_doc_bytes_read" {
 resource "aws_cloudwatch_log_group" "log1" {
   name = "Log1"
 }*/
+resource "aws_cloudformation_stack" "network" {
+  name = "networking-stack"
 
+  parameters = {
+    VPCCidr = "10.0.0.0/16"
+  }
+
+  template_body = <<STACK
+{
+  "Parameters" : {
+    "VPCCidr" : {
+      "Type" : "String",
+      "Default" : "10.0.0.0/16",
+      "Description" : "Enter the CIDR block for the VPC. Default is 10.0.0.0/16."
+    }
+  },
+  "Resources" : {
+    "myVpc": {
+      "Type" : "AWS::EC2::VPC",
+      "Properties" : {
+        "CidrBlock" : { "Ref" : "VPCCidr" },
+        "Tags" : [
+          {"Key": "Name", "Value": "Primary_CF_VPC"}
+        ]
+      }
+    }
+  }
+}
+STACK
+}
