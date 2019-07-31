a2ceb301a23192afb46e78b9fa1d4a767ce58cf8
Yujia Cui <yujia.cui@ucalgary.ca>
2019-07-31 13:19:28 -0600

    better formatted, dynamically partitions the things

--- resources.tf
+++ resources.tf
@@ -4,32 +4,66 @@ resource "aws_vpc" "vpc_environment" {
   enable_dns_support   = true
 }
 
-resource "aws_subnet" "public_subnet1" {
-  cidr_block              = cidrsubnet(aws_vpc.vpc_environment.cidr_block, 4, 1)
+resource "aws_subnet" "public_subnet" {
+  count = length(split(",",var.availability_zones))
   vpc_id                  = aws_vpc.vpc_environment.id
-  availability_zone       = "us-west-2c"
+  availability_zone = split(",",var.availability_zones)[count.index]
+  cidr_block        = cidrsubnet(signum(length(aws_vpc.vpc_environment.cidr_block)) == 1 ? aws_vpc.vpc_environment.cidr_block : aws_vpc.vpc_environment.cidr_block, ceil(log(length(var.availability_zones) * 2, 2)), length(var.availability_zones) + count.index)
+//  cidr_block              = "10.${var.cidr_numeral}.${lookup(var.cidr_numeral_public, count.index)}.0/24"
+//  cidr_block = cidrsubnet(aws_vpc.vpc_environment.cidr_block,8,count.index+4)
   map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "private_subnet1" {
-  cidr_block        = cidrsubnet(aws_vpc.vpc_environment.cidr_block, 4, 2)
-  vpc_id            = aws_vpc.vpc_environment.id
-  availability_zone = "us-west-2c"
+//routing table for public subnets
+resource "aws_route_table" "route-public" {
+  vpc_id = aws_vpc.vpc_environment.id
+  //depends_on = [aws_vpc.vpc_environment]
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.default_gat.id
+  }
+
+  tags = {
+    Name = "vpc Routing Table"
+  }
 }
 
-resource "aws_subnet" "public_subnet2" {
-  cidr_block              = cidrsubnet(aws_vpc.vpc_environment.cidr_block, 4, 3)
+resource "aws_main_route_table_association" "main_table_assoc" {
+  vpc_id         = aws_vpc.vpc_environment.id
+  route_table_id = aws_route_table.route-public.id
+}
+
+resource "aws_route_table_association" "subnet1_table_assoc" {
+  count = length(split(",",var.availability_zones))
+  subnet_id      = aws_subnet.public_subnet.*.id[count.index]
+  //  aws_subnet.*.id,count.index
+  route_table_id = aws_route_table.route-public.id
+}
+
+//private subnets
+resource "aws_subnet" "private_subnet" {
+  count = length(split(",",var.availability_zones))
   vpc_id                  = aws_vpc.vpc_environment.id
-  availability_zone       = "us-west-2b"
+  cidr_block        = cidrsubnet(signum(length(aws_vpc.vpc_environment.cidr_block)) == 1 ? aws_vpc.vpc_environment.cidr_block: aws_vpc.vpc_environment.cidr_block, ceil(log(length(var.availability_zones) * 2, 2)), count.index)
+//  cidr_block ="10.${var.cidr_numeral}.${lookup(var.cidr_numeral_private,count.index )}"
+//  cidr_block = cidrsubnet(aws_vpc.vpc_environment.cidr_block,7,count.index)
+//  cidr_block ="10.${var.cidr_numeral}.${lookup(var.cidr_numeral_private,count.index )}"
+
+  availability_zone = split(",",var.availability_zones)[count.index]
   map_public_ip_on_launch = true
+  // not sure if we need this line
+  depends_on = ["aws_internet_gateway.default_gat"]
 }
 
-resource "aws_subnet" "private_subnet2" {
-  cidr_block        = cidrsubnet(aws_vpc.vpc_environment.cidr_block, 4, 4)
-  vpc_id            = aws_vpc.vpc_environment.id
-  availability_zone = "us-west-2b"
+resource "aws_route_table_association" "private" {
+  count          = length(split(",", var.availability_zones))
+//  subnet_id      = aws_subnet.private_subnet.*.id[count.index]
+//  route_table_id = aws_route_table.route-private1[count.index]
+  subnet_id      = aws_subnet.private_subnet.*.id[count.index]
+  route_table_id = aws_route_table.route-private1.*.id[count.index]
 }
 
+//
 resource "aws_security_group" "private_subnesecurity" {
   vpc_id = aws_vpc.vpc_environment.id
   ingress {
@@ -99,27 +133,16 @@ resource "aws_internet_gateway" "default_gat" {
   vpc_id = aws_vpc.vpc_environment.id
 }
 
-//routing table for public subnets
-resource "aws_route_table" "route-public" {
-  vpc_id = aws_vpc.vpc_environment.id
-//depends_on = [aws_vpc.vpc_environment]
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.default_gat.id
-  }
 
-  tags = {
-    Name = "vpc Routing Table"
-  }
-}
 
 //routing table for private subnet 1
 resource "aws_route_table" "route-private1" {
+  count = length(split(",", var.availability_zones))
   vpc_id = aws_vpc.vpc_environment.id
 //  depends_on = [aws_subnet.private_subnet1]
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat_1gate.id
+    gateway_id = aws_internet_gateway.default_gat.id
   }
 
   tags = {
@@ -127,46 +150,39 @@ resource "aws_route_table" "route-private1" {
   }
 }
 
-//routing table for private subnet 2
-resource "aws_route_table" "route-private2" {
-  vpc_id = aws_vpc.vpc_environment.id
+////routing table for private subnet 2
+//resource "aws_route_table" "route-private2" {
+//  vpc_id = aws_vpc.vpc_environment.id
+//
+//  route {
+//    cidr_block = "0.0.0.0/0"
+//    gateway_id = aws_nat_gateway.nat_1gate.id
+//  }
+//
+//  tags = {
+//    Name = "Private Subnet 2 Routing Table"
+//  }
+//}
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat_1gate.id
-  }
 
-  tags = {
-    Name = "Private Subnet 2 Routing Table"
-  }
-}
-
-resource "aws_main_route_table_association" "main_table_assoc" {
-  vpc_id         = aws_vpc.vpc_environment.id
-  route_table_id = aws_route_table.route-public.id
-}
-
-resource "aws_route_table_association" "subnet1_table_assoc" {
-  subnet_id      = aws_subnet.private_subnet1.id
-  route_table_id = aws_route_table.route-private1.id
-}
-resource "aws_route_table_association" "subnet2_table_assoc" {
-  subnet_id      = aws_subnet.private_subnet2.id
-  route_table_id = aws_route_table.route-private2.id
-}
+//resource "aws_route_table_association" "subnet2_table_assoc" {
+//  subnet_id      = aws_subnet.private_subnet2.id
+//  route_table_id = aws_route_table.route-private2.id
+//}
 variable "ami" {
 
   default = "ami-07669fc90e6e6cc47"
 }
 resource "aws_launch_configuration" "autoscale_launch_config" {
-  name_prefix          = "autoscale_launcher-Craig-"
+  name_prefix          = "autoscale_launcher-nikky-"
   image_id        = var.ami
   instance_type   = "t2.nano"
 //  key_name        = var.ami_key_pair_name
   security_groups = [aws_security_group.security.id]
   enable_monitoring = true
   user_data = file(
-    "C:/Users/Default.Default-PC/Downloads/install_apache_server.sh"
+    "/Users/yujiacui/Desktop/install_apache_server.sh"
+
   )
   lifecycle {create_before_destroy = true}
 }
@@ -200,14 +216,8 @@ variable "max_asg2" {
 resource "aws_autoscaling_group" "autoscale_group_1" {
   name="asg-${aws_launch_configuration.autoscale_launch_config.name}"
   launch_configuration = aws_launch_configuration.autoscale_launch_config.id
-  vpc_zone_identifier  = [aws_subnet.private_subnet2.id, aws_subnet.private_subnet1.id]
-  /*
-    initial_lifecycle_hook {
-      lifecycle_transition = "autoscaling:EC2_INSTANCE_TERMINATING"
-      heartbeat_timeout = 7200
-
-      name = "delay"
-    }*/
+//  vpc_zone_identifier  = [aws_subnet.private_subnet2.id, aws_subnet.private_subnet1.id]
+  vpc_zone_identifier  = aws_subnet.public_subnet.*.id
   min_size = var.min_asg
   max_size = var.max_asg
   desired_capacity = var.des_asg
@@ -215,7 +225,7 @@ resource "aws_autoscaling_group" "autoscale_group_1" {
 
   tag {
     key                 = "Name"
-    value               = "auto_scale-Craig"
+    value               = "auto_scale-Nikky"
     propagate_at_launch = true
   }
 
@@ -232,17 +242,17 @@ resource "aws_autoscaling_group" "autoscale_group_1" {
   //depends_on = [data.aws_autoscaling_group.autoscale_group_1]
   metrics_granularity="1Minute"
 provisioner "local-exec" {
-  command = "check_health.sh ${aws_alb.alb.dns_name} asg-autoscale_launcher-Craig-20190724010048552300000001 aws_alb_target_group.alb_target_group_1.arn"
+  command = "check_health.sh ${aws_alb.alb.dns_name} asg-autoscale_launcher-nikky-20190724010048552300000001 aws_alb_target_group.alb_target_group_1.arn"
 }
 }
 
-data "aws_autoscaling_group" "autoscale_group_1" {
-  name = aws_autoscaling_group.autoscale_group_1.name
-}
-output "autoscalingname" {
-  value = [data.aws_autoscaling_group.autoscale_group_1.name, aws_alb_target_group.alb_target_group_1.arn, aws_autoscaling_group.autoscale_group_1.id]
-
-}
+//data "aws_autoscaling_group" "autoscale_group_1" {
+//  name = aws_autoscaling_group.autoscale_group_1.name
+//}
+//output "autoscalingname" {
+//  value = [data.aws_autoscaling_group.autoscale_group_1.name, aws_alb_target_group.alb_target_group_1.arn, aws_autoscaling_group.autoscale_group_1.id]
+//
+//}
 //data "terraform_remote_state" "vpc" {
 //  backend = "local"
 //}
@@ -251,35 +261,7 @@ output "autoscalingname" {
   ASGname=aws_autoscaling_group.autoscale_group_1.name
 }
 
-/*
-resource "aws_autoscaling_policy" "web_policy_up" {
-  name = "web_policy_up"
-  scaling_adjustment = 1
-  adjustment_type = "ChangeInCapacity"
-  cooldown = 300
-  autoscaling_group_name = "${aws_autoscaling_group.autoscale_group_1.name}"
-//  autoscaling_group_name = "${aws_autoscaling_group.web.name}"
-  lifecycle {create_before_destroy = true}
-}
-resource "aws_cloudwatch_metric_alarm" "web_cpu_alarm_up" {
-  alarm_name = "web_cpu_alarm_up"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods = "2"
-  metric_name = "CPUUtilization"
-  namespace = "AWS/EC2"
-  period = "120"
-  statistic = "Average"
-  threshold = "60"
-  lifecycle {create_before_destroy = true}
-  dimensions = {
-    AutoScalingGroupName = "${aws_autoscaling_group.autoscale_group_1.name}"
-//    "${aws_autoscaling_group.web.name}"
-  }
 
-  alarm_description = "This metric monitor EC2 instance CPU utilization"
- // alarm_actions = ["${aws_autoscaling_policy.web_policy_up.arn}"]
-}
-*/
 resource "aws_alb_target_group" "alb_target_group_1" {
   name_prefix    = "targp-"
   port     = "80"
@@ -314,16 +296,14 @@ resource "aws_autoscaling_attachment" "alb_autoscale" {
   autoscaling_group_name = aws_autoscaling_group.autoscale_group_1.id
   lifecycle {create_before_destroy = true}
   provisioner "local-exec" {
-    command = "attchmnt_checkhealth.sh asg-autoscale_launcher-Craig-20190725064518336300000001 ${aws_alb_target_group.alb_target_group_1.arn}"
+    command = "attchmnt_checkhealth.sh asg-autoscale_launcher-nikky-20190725064518336300000001 ${aws_alb_target_group.alb_target_group_1.arn}"
   }
 
 }
 
 resource "aws_alb" "alb" {
   name_prefix = "lbCrg-"
-  subnets = [
-    aws_subnet.public_subnet1.id,
-    aws_subnet.public_subnet2.id]
+  subnets =aws_subnet.public_subnet.*.id
   security_groups = [
     aws_security_group.security.id]
   internal = false
@@ -350,111 +330,55 @@ resource "aws_alb_listener" "alb_listener" {
 
 
 resource "aws_nat_gateway" "nat_2gate" {
-  allocation_id = aws_eip.nat_eip2.id
-  subnet_id     = aws_subnet.public_subnet2.id
-  lifecycle {
-    create_before_destroy = true
-  }
-}
 
-resource "aws_nat_gateway" "nat_1gate" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_subnet1.id
+  count  = length(split(",", var.availability_zones))
+
+  allocation_id = aws_eip.nat_eip2.*.id[count.index]
+  subnet_id = aws_subnet.public_subnet.*.id[count.index]
   lifecycle {
     create_before_destroy = true
   }
 }
 
 resource "aws_eip" "nat_eip2" {
-
+  count = length(split(",", var.availability_zones))
   vpc      = true
   lifecycle {
     create_before_destroy = true
   }
 }
-resource "aws_eip" "nat_eip" {
-
-  vpc      = true
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-resource "aws_route_table" "routtable1" {
-
-  vpc_id = aws_vpc.vpc_environment.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_1gate.id
-  }
-
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-resource "aws_route_table" "routtable2" {
-
-  vpc_id = aws_vpc.vpc_environment.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_2gate.id
-  }
-
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-/*
-resource "aws_cloudwatch_log_metric_filter" "viewer_view_doc_bytes_read" {
-  name           = "viewer_view_document_count"
-  pattern        = ""
-  log_group_name = aws_cloudwatch_log_group.log1.name
-  metric_transformation {
-    name         = "viewer_view_doc_bytes_read"
-    namespace    = "LogMetrics"
-    value        = 1
-  }
-}
-resource "aws_cloudwatch_log_group" "log1" {
-  name = "Log1"
-}*/
-
-
-/*
-resource "aws_cloudformation_stack" "network" {
-  name = "networking-stack"
-
-  parameters = {
-    VPCCidr = "10.0.0.0/16"
-  }
-
-  template_body = <<STACK
-{
-  "Parameters" : {
-    "VPCCidr" : {
-      "Type" : "String",
-      "Default" : "10.0.0.0/16",
-      "Description" : "Enter the CIDR block for the VPC. Default is 10.0.0.0/16."
-    }
-  },
-  "Resources" : {
-    "myVpc": {
-      "Type" : "AWS::EC2::VPC",
-      "Properties" : {
-        "CidrBlock" : { "Ref" : "VPCCidr" },
-        "Tags" : [
-          {"Key": "Name", "Value": "Primary_CF_VPC"}
-        ]
-      }
-    }
-  }
-}
-STACK
-}
-
-
-
-*/
+//resource "aws_eip" "nat_eip" {
+//
+//  vpc      = true
+//  lifecycle {
+//    create_before_destroy = true
+//  }
+//}
+//resource "aws_route_table" "routtable1" {
+//
+//  vpc_id = aws_vpc.vpc_environment.id
+//
+//  route {
+//    cidr_block = "0.0.0.0/0"
+//    nat_gateway_id = aws_nat_gateway.nat_1gate.id
+//  }
+//
+//
+//  lifecycle {
+//    create_before_destroy = true
+//  }
+//}
+//resource "aws_route_table" "routtable2" {
+//
+//  vpc_id = aws_vpc.vpc_environment.id
+//
+//  route {
+//    cidr_block = "0.0.0.0/0"
+//    nat_gateway_id = aws_nat_gateway.nat_2gate.id
+//  }
+//
+//
+//  lifecycle {
+//    create_before_destroy = true
+//  }
+//}
