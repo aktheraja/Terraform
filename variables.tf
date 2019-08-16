bf10ba41c3fc4fa5feec98e0fda6111a846469e1
Craig Martis <craigmartis@yahoo.ca>
2019-08-16 11:23:32 -0600

    Added variable to control first-time apply after a destroy

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "deployment_name"{
 }
 
 variable "user_data_file_string"{
-  default = "C:/Users/Default.Default-PC/Downloads/install_apache_server.sh"
+  default = "C:/Users/Default.Default-PC/Downloads/install_apache_server2.sh"
 }
 
 variable "min_asg" {
@@ -25,7 +25,7 @@ variable "max_asg" {
 }
 
 variable "ASG_health_check_grace"{
-  default=200
+  default = 200
 }
 
 variable "ASG_health_check_type"{
@@ -40,65 +40,57 @@ variable "instance_type" {
   default = "t2.nano"
 }
 
-//variable "user_data_change1"{
-//  default = true
-//}
-
 variable "always_switch" {
-  default=false
+  default = false
 }
 
-
+variable "first_time_create" {
+  default = false
+}
 
 locals {
  ASG1_is_active = tobool(chomp(file(".ASG1Active.txt")))
  old_user_data =  chomp(file(".UserData.txt"))
  old_ami =  chomp(file(".AMI.txt"))
-  //switching = var.switch?1:null
- //new_LC_status = chomp(file("switchstatus.txt"))
-  new_LC=local.old_user_data!=var.user_data_file_string||local.old_ami!=var.ami?true:false
+ new_LC=local.old_user_data!=var.user_data_file_string||local.old_ami!=var.ami?true:false
  change_to_ASG_2=(var.always_switch==false&&local.ASG1_is_active==local.new_LC)||(var.always_switch&&local.ASG1_is_active)?true:false
  ASG1_max = local.change_to_ASG_2?0:var.max_asg
  ASG1_min = local.change_to_ASG_2?0:var.min_asg
  ASG2_max = local.change_to_ASG_2?var.max_asg:0
  ASG2_min = local.change_to_ASG_2?var.min_asg:0
-//  ASG1_depends_on=local.change_to_ASG_2?[aws_autoscaling_group.autoscale_group_2, aws_autoscaling_attachment.alb_autoscale_2, null_resource.change_detected]:[null_resource.change_detected]
-//  ASG2_depends_on=local.change_to_ASG_2?[null_resource.change_detected]:[aws_autoscaling_attachment.alb_autoscale_1, aws_autoscaling_group.autoscale_group_1, null_resource.change_detected]
-
 }
-//
-//output "LC_user_data" {
-//  value = aws_launch_configuration.autoscale_launch_config1.user_data
-//}
-//
-output "raw_user_data" {
+
+output "old_raw_user_data" {
   value = local.old_user_data
 }
-output "switchstatus" {
-  value = [local.new_LC]
+
+output "new_raw_user_data" {
+  value = var.user_data_file_string
 }
-//
-output "OIA_status" {
+output "ASG1Max" {
+  value = local.ASG1_max
+}
+output "ASG1Min" {
+  value = local.ASG1_min
+}
+output "ASG2Max" {
+  value = local.ASG2_max
+}
+output "ASG2Min" {
+  value = local.ASG2_min
+}
+output "always_switch" {
+  value = var.always_switch
+}
+output "switch_due_to_data" {
+  value = local.new_LC
+}
+output "ASG1_is_active" {
   value = local.ASG1_is_active
 }
-output "changeToASG2" {
+output "change_to_ASG2" {
   value = local.change_to_ASG_2
 }
-//output "ASG1Max" {
-//  value = local.ASG1_max
-//}
-//output "ASG1Min" {
-//  value = local.ASG1_min
-//}
-//output "ASG2Max" {
-//  value = local.ASG2_max
-//}
-//output "ASG2Min" {
-//  value = local.ASG2_min
-//}
-//output "NS_status" {
-//  value = local.new_LC_status
-//}
-//output "DS_status" {
-//  value = var.autoswitch
-//}
+output "first_time_create" {
+  value = var.first_time_create
+}
