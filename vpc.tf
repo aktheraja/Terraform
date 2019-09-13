8d7c06fb51a50a12091f1d8b12afd5b8b00894bc
aktheraja <wale.akin2007@gmail.com>
2019-09-13 11:38:56 -0600

    updated

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "vpc_environment" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/24"
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags ={
